9427403e3b0a96d2d9c37b08e5b16cc23d3cd36a
jonhuseinzoda <shuseinzoda@yahoo.com>
2023-11-03 19:02:53 -0500

    Done with my US

--- src/test/java/com/symund/pages/FilesPage.java
+++ src/test/java/com/symund/pages/FilesPage.java
@@ -2,17 +2,51 @@ package com.symund.pages;
 
 import com.symund.utilities.Driver;
 import org.openqa.selenium.By;
+
+
+import org.openqa.selenium.support.FindBy;
+
+
+import java.awt.*;
+import java.util.ArrayList;
+
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 
 import java.util.List;
 
 public class FilesPage {
 
-    public FilesPage(){
+    public FilesPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+    @FindBy(xpath ="//a[@aria-label='Files']")
+    public WebElement FilesButton;
+    @FindBy(linkText = "Name")
+    public WebElement NameArrowButton;
+
+    @FindBy(xpath = "//tr[@data-type='dir' and @data-favorite='true']")
+    public List<WebElement> AllFavoriteFolders;
+    @FindBy(xpath = "//tr[@data-type='file' and @data-favorite='true']")
+    public List<WebElement> AllFavoriteFiles;
+
+@FindBy(xpath = "//tr[@data-type='dir' and not(contains(@data-favorite, 'true'))]")
+public List<WebElement> notFavoriteFolders;
+@FindBy (xpath = "//tr[@data-type='file' and not(contains(@data-favorite, 'true'))]")
+public List<WebElement> notFavoriteFiles;
+
+
+
+
+
+
+
+
+
 
     public static WebElement selectTab(String tabName) {
         WebElement header;
@@ -22,16 +56,33 @@ public class FilesPage {
             return null; // Return null if the module is not found.
         }
 
-
         List<WebElement> tabs = header.findElements(By.tagName("a"));
         for (WebElement eachTab : tabs) {
-            if (eachTab.getText().contains(tabName.substring(0,1).toUpperCase())){ // will change the 1st letter of the tabName to UpperCase
+            if (eachTab.getText().contains(tabName.substring(0, 1).toUpperCase())) { // will change the 1st letter of the tabName to UpperCase
                 return eachTab; // Return the first displayed module.
             }
         }
 
         return null; // Return null if no displayed modules are found.
     }
+
+    public static boolean isElementPresent(WebDriver driver, By by) {
+        try {
+            Driver.getDriver().findElement(by);
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+
+
+
+
+
+    @FindBy(xpath = "//a[.='Deleted']")
+    public WebElement sortByDeleted;
+
+
 
 
 }
