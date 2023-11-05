901509a828795b01922b5b4b8fe779c7de253773
raziaagiseva <razia221175@gmail.com>
2023-11-05 00:44:55 -0400

    Added some locators

--- src/test/java/com/symund/pages/FilesPage.java
+++ src/test/java/com/symund/pages/FilesPage.java
@@ -1,13 +1,12 @@
 package com.symund.pages;
 
 import com.symund.utilities.Driver;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.awt.*;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class FilesPage {
